82088bb4a6849e3c07ae5648fd7fd5bcc30f0712
Thiago G. Quinto <thiagogarianiq@gmail.com>
2025-11-09 21:24:45 -0300

    fix

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main(input_json: str, out_json: str, conf: Dict, repos_dir: str):
 
         bug_inducing_commits = set()
         # repo_name = commit['repo_name']
-        repo_name = "JabRef/jabref"  # for testing purposes
+        repo_name = "jabref"  # for testing purposes
         repo_url = f'https://test:test@github.com/{repo_name}.git'  # using test:test as git login to skip private repos during clone
         fix_commit = commit['fix_commit_hash']
 
